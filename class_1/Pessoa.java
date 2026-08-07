79233125f50430b439eb51b86e33afad77b4fe33
Gabriela Rodrigues <gabriela.souza.investimentos@gmail.com>
2026-08-07 00:09:03 +0000

    feat:Incluindo comentários

--- class_1/Pessoa.java
+++ class_1/Pessoa.java
@@ -1,13 +1,14 @@
 public class Pessoa{
-    String nome;
-    int idade;
+    String nome; //Atributo//
+    int idade;  //Atributo//
 
     public static void main(String[] args){
-        Pessoa pessoa = new Pessoa();
+        Pessoa pessoa = new Pessoa(); //Instanciando a classe//
 
-        pessoa.nome = "Gabriela";
-        pessoa.idade = 20;
+        pessoa.nome = "Gabriela"; //Atribuindo valor ao atributo nome//
+        pessoa.idade = 20; //Atribuindo valor ao atributo idade//
 
-        System.out.println("Nome:" + pessoa.nome + "\nIdade:" + pessoa.idade);
+        System.out.println("Nome:" + pessoa.nome + "\nIdade:" + pessoa.idade); //Exibindo mensagem com os valores//
     }
 }
+
