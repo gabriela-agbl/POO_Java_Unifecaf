8e11ed8ba57aa3c6045c2522b19dfa656ac7bffe
Gabriela Rodrigues <gabriela.souza.investimentos@gmail.com>
2026-08-13 23:30:37 +0000

    feat: criando GET e SET dentro da classe Pessoa

--- class_2/Pessoa/Main.java
+++ class_2/Pessoa/Main.java
@@ -1,11 +1,16 @@
 public class Main{
     public static void main(String[] args){
         Pessoa pessoa = new Pessoa("Maria", 20);
-        Pessoa pessoa2 = new Pessoa("Mario", 19);
 
-        pessoa.idade = 400;
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+
+        pessoa.setNome("Olivia");
+        pessoa.setIdade(70);
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
 
         pessoa.apresentar();
-        pessoa2.apresentar();
     }
 }
--- class_2/Pessoa/Pessoa.java
+++ class_2/Pessoa/Pessoa.java
@@ -7,6 +7,24 @@ public class Pessoa{
         this.idade = idade;
     }
 
+    //GET and SET
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
     void apresentar(){
         System.out.println("Ola! Meu nome é " + nome + "\nTenho " + idade + " anos!");
     }
