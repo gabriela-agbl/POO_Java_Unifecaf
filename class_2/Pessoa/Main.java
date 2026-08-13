c0710b284470a936ef695791db509feee6e63154
Gabriela Rodrigues <gabriela.souza.investimentos@gmail.com>
2026-08-13 22:47:12 +0000

    feat: separando main da classe criada

---
+++ class_2/Pessoa/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.nome = "Maria";
+        pessoa.idade = 34;
+
+        pessoa.apresentar();
+    }
+}
---
+++ class_2/Pessoa/Pessoa.java
@@ -0,0 +1,8 @@
+public class Pessoa{
+    String nome;
+    int idade;
+
+    void apresentar(){
+        System.out.println("Ola! Meu nome é " + nome);
+    }
+}
