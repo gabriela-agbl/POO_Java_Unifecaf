93fd5e895352b857039aac6a9ebd2707dc3d0043
Gabriela Rodrigues <gabriela.souza.investimentos@gmail.com>
2026-09-03 22:57:57 +0000

    feat: Revisando herança

---
+++ class_5/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+	protected String nome;
+	
+	public Animal(String nome) {
+		this.nome = nome;
+	}
+	
+	public void apresentar() {
+		System.out.println("Animal: " +nome);
+	}
+	
+	public void emitir_som() {
+		System.out.println("O animal emitiu um som");
+	}
+}
---
+++ class_5/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Cachorro cachorro = new Cachorro("Dog");
+		
+		cachorro.apresentar();
+		cachorro.emitir_som();
+	}
+
+}
